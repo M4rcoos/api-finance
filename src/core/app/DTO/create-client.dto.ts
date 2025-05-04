ff8a8ea5d2138c036a24f36c37d1351be0c87226
M4rcoos <contato.shopvmax@gmail.com>
2025-05-04 00:34:29 -0300

    refact:🔨 Refatorando Dto e Schema de Clients

--- src/core/app/DTO/create-client.dto.ts
+++ src/core/app/DTO/create-client.dto.ts
@@ -2,7 +2,18 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateClientDto {
-  @ApiProperty({ example: "John Doe" })
+  @ApiProperty({ example: "Marcos" })
   @IsNotEmpty()
   name: string;
+  
+  @ApiProperty({ example: "Vinicius" })
+  @IsNotEmpty()
+  surname: string;
+
+  @ApiProperty({ example: "Vinicius" })
+  phone: string;
+
+  @ApiProperty({example:1})
+  @IsNotEmpty()
+  owner_id:number
 }
