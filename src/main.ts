396de03878eb141868fa00d0931f019655257b6e
M4rcoos <contato.shopvmax@gmail.com>
2025-05-20 23:39:55 -0300

    fix: corrigindo prisma para deploy

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
- 
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true, 
-      transform: true, 
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api/docs", app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
